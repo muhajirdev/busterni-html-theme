e8be2eb3c0d710e85ac05aa8a16861fb08db7abe
Muhammad Muhajir <muhajirframe@gmail.com>
2016-01-17 18:43:42 +0700

    Release

---
+++ js/init-home.js
@@ -0,0 +1,12 @@
+ $(".button-collapse").sideNav();
+$(window).scroll(function(event){
+  $("#logo-container").fadeIn();
+  $(".navbar-fixed nav").removeClass("z-depth-0");
+})
+$( ".courses > div" ).hover(
+  function() {
+    $( ".courses ").addClass ( "z-depth-0");
+  }, function() {
+    $( ".courses").removeClass ("z-depth-0")
+  }
+);
